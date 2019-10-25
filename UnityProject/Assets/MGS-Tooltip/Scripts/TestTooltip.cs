c065848555604f07f4ab6c4765c89b7fe4d8a05f
Mogoson <mogoson@outlook.com>
2019-10-26 00:03:07 +0800

    Fixed ui form manager.

---
+++ UnityProject/Assets/MGS-Tooltip/Scripts/TestTooltip.cs
@@ -0,0 +1,28 @@
+﻿/*************************************************************************
+ *  Copyright © 2019 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  TestTooltip.cs
+ *  Description  :  Null.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  25/10/2019
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using MGS.UIForm;
+using UnityEngine;
+
+namespace MGS.Tooltip
+{
+    [AddComponentMenu("MGS/Tooltip/TestTooltip")]
+    public class TestTooltip : MonoBehaviour
+    {
+        #region Private Method
+        private void Start()
+        {
+            UIFormManager.Instance.OpenForm<TestTooltipForm>();
+        }
+        #endregion
+    }
+}
---
+++ UnityProject/Assets/MGS-Tooltip/Scripts/TestTooltipForm.cs
@@ -0,0 +1,35 @@
+﻿/*************************************************************************
+ *  Copyright © 2019 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  TestTooltipForm.cs
+ *  Description  :  Null.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  25/10/2019
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using MGS.UIForm;
+using UnityEngine;
+
+namespace MGS.Tooltip
+{
+    [AddComponentMenu("MGS/Tooltip/TestTooltipForm")]
+    public class TestTooltipForm : MonoUIForm
+    {
+        #region Private Method
+        private void Start()
+        {
+            UIFormManager.Instance.OpenForm<TestTooltipForm>();
+        }
+        #endregion
+
+        #region Public Method
+        public override bool Refresh(object data)
+        {
+            return true;
+        }
+        #endregion
+    }
+}
